833e96bebcf8958607545eb7a97c2ae9049c2142
Chimpook Virtuch <chimpook@gmail.com>
2022-05-05 17:04:36 +0200

    Easter eggs: two variants of requests

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,21 +3,45 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(string $greet = ""): Response
     {
         return new Response(<<<EOF
 <html>
     <body>
+        $greet
         <img src="/images/under-construction.gif" />
     </body>
 </html>
 EOF
     );
     }
+
+    #[Route('/hello_there', name: 'hello there')]
+    public function hello_there(Request $request): Response
+    {
+        $greet = '';
+        $name = $request->query->get('name');
+        if ($name) {
+            $greet = sprintf('<h1>Hello there, %s</h1>', htmlspecialchars($name));
+        }
+        return $this->index($greet);
+    }
+
+    #[Route('hello/{name}', name: 'hello')]
+    public function hello(string $name = ''): Response
+    {
+        $greet = '';
+        if ($name) {
+            $greet = sprintf('<h1>Hello, %s</h1>', htmlspecialchars($name));
+        }
+        return $this->index($greet);
+    }
 }
+
