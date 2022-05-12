1f3f4024f4abb88643bdbb58682231d23108eb3b
Chimpook Virtuch <chimpook@gmail.com>
2022-05-12 15:39:18 +0200

    Make migration

--- migrations/Version20220512133641.php
+++ migrations/Version20220512133641.php
@@ -20,7 +20,14 @@ final class Version20220512133641 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-
+        $this->addSql('
+            CREATE TABLE sessions (
+                sess_id VARCHAR(128) NOT NULL PRIMARY KEY,
+                sess_data BYTEA NOT NULL,
+                sess_lifetime INTEGER NOT NULL,
+                sess_time INTEGER NOT NULL
+            )
+        ');
     }
 
     public function down(Schema $schema): void
