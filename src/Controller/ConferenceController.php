818b6a8f2c48319062e5d7a25c3c60bdd6f95afd
Chimpook Virtuch <chimpook@gmail.com>
2022-05-15 16:33:25 +0200

    Processing files uploaded with the form

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -13,6 +13,7 @@ use App\Repository\CommentRepository;
 use App\Entity\Comment;
 use App\Form\CommentFormType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class ConferenceController extends AbstractController
 {
@@ -34,7 +35,7 @@ class ConferenceController extends AbstractController
     }
 
     #[Route('/conference/{slug}', name: 'conference')]
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, string $photoDir): Response
     {
         $comment = new Comment();
         $form = $this->createForm(CommentFormType::class, $comment);
@@ -42,6 +43,17 @@ class ConferenceController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $comment->setConference($conference);
+            if ($photo = $form['photo']->getData()) {
+                $filename = bin2hex(random_bytes(6)) . '.' . $photo->guessExtension();
+
+                try {
+                    $photo->move($photoDir, $filename);
+                } catch (FileException $e) {
+                    // unable to upload the photo, give up
+                }
+                $comment->setPhotoFilename('/uploads/photos/' . $filename);
+            }
+
             $this->entityManager->persist($comment);
             $this->entityManager->flush();
 
